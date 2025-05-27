daad1c5e728de318e4483d5b7871a7aef548b6b9
dev-ggomes <gomesguilherme.pt@gmail.com>
2025-05-27 10:14:20 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("--headless"); // Executa o Chrome em modo invisível
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--window-size=1920,1080");
+
+        using (IWebDriver driver = new ChromeDriver(options))
+        {
+            while (true)
+            {
+                driver.Navigate().GoToUrl("https://site.com");
+                Console.WriteLine($"Página carregada às {DateTime.Now}");
+
+                Thread.Sleep(5000); // Espera 5 segundos antes de recarregar
+            }
+        }
+    }
+}
